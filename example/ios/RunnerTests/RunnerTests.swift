1a8ea80c6f9fdef6e3113383ad3e3f7a136dacfb
moven0831 <moven0831@gmail.com>
2025-04-17 12:40:58 +0800

    test: add unit test for Circom prover and verifier with zkey file

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -2,7 +2,6 @@ import Flutter
 import UIKit
 import XCTest
 
-
 @testable import mopro_flutter_package
 
 // This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
@@ -23,5 +22,33 @@ class RunnerTests: XCTestCase {
     }
     waitForExpectations(timeout: 1)
   }
+
+   func testCircomProverAndVerifier() async throws {
+     guard let zkeyPath = Bundle(for: RunnerTests.self).path(forResource: "multiplier2_final", ofType: "zkey") else {
+       XCTFail("zkey path not found in the test bundle")
+       return
+     }
+     do {
+       let a = 3
+       let b = 5
+       let c = a * b
+       let input_str: String = "{\"b\":[\"5\"],\"a\":[\"3\"]}"
+
+       let outputs: [String] = [String(c), String(a)]
+
+       let generateProofResult = try generateCircomProof(
+         zkeyPath: zkeyPath, circuitInputs: input_str, proofLib: ProofLib.arkworks)
+
+       let isValid = try verifyCircomProof(
+         zkeyPath: zkeyPath,
+         proofResult: CircomProofResult(
+           proof: generateProofResult.proof, inputs: generateProofResult.inputs),
+         proofLib: ProofLib.arkworks)
+       XCTAssertTrue(isValid, "Proof verification should succeed")
+       XCTAssertEqual(outputs, generateProofResult.inputs, "Generated inputs should match expected outputs")
+     } catch {
+       XCTFail("testCircomProverAndVerifier failed with error: \(error)")
+     }
+   }
 
 }
